f65201e89693d6172135082decbb05500d85e58a
Copecofee <84367018+Copecofee@users.noreply.github.com>
2022-07-18 12:54:32 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,21 @@
+#!/bin/bash
+#
+# install.sh - Install the dependencies
+#
+# Made by: Copecofee Lovegood
+# Manutention: Copecofee
+#
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+# History:
+#
+#  v1.0 Copecofee:
+#    - The Script Was created
+#  v1.1  
+#    - Adding comments
+#    - Making the script a program
+#    - Organazing the code
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
 apt update
 apt install git
 pip install colorama
