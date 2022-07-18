e00cdebe42c2147cdb820ee2ab8cd08a14456438
Copecofee <84367018+Copecofee@users.noreply.github.com>
2022-07-18 12:59:21 -0300

    Update installtermux.sh

--- installtermux.sh
+++ installtermux.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+#
+# install.sh - Install the dependencies for Termux
+#
+# Made by: Copecofee Lovegood
+# Manutention: Copecofee
+#
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+# History:
+#
+#  v1.0 Copecofee:
+#    - The Script Was created
+#  v1.1  07-18-2022, Copecofee:
+#    - Adding comments
+#    - Making the script a program
+#    - Organazing the code
+#
+#-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
 pkg update && pkg upgrade
 pkg install golang
 pip install colorama
